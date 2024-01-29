bca45df57fc85f8ad51b3908ff4fd16a04e64dac
Alex <alex@fufelshmits.inc>
2024-01-29 20:44:11 +0200

    added ttf to woff2 on demand

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,36 +1,36 @@
-import * as nodePath from 'path';
+import * as nodePath from "path";
 const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = `./dist/`;
 const srcFolder = `./src`;
 
 export const path = {
-  build:{
+  build: {
     html: `${buildFolder}/`,
     css: `${buildFolder}/assets/css/`,
     js: `${buildFolder}/assets/js/`,
     files: `${buildFolder}/assets/files/`,
     images: `${buildFolder}/assets/img/`,
-    fonts: `${buildFolder}/assets/fonts/`,
+    fonts: `${srcFolder}/files/fonts/`
   },
   src: {
     html: `${srcFolder}/*.html`,
     scss: `${srcFolder}/scss/style.scss`,
     js: `${srcFolder}/js/script.js`,
     files: `${srcFolder}/files/**/*.*`,
-    images:`${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
-    svg: `${srcFolder}/img/**/*.svg`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`
   },
   watch: {
     html: `${srcFolder}/**/*.html`,
     scss: `${srcFolder}/scss/**/*.scss`,
     js: `${srcFolder}/js/**/*.js`,
     files: `${srcFolder}/files/**/*.*`,
-    images:`${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`
   },
   clean: buildFolder,
   buildFolder: buildFolder,
   srcFolder: srcFolder,
   rootFolder: rootFolder,
-  ftp: ``,
+  ftp: ``
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ import less from "less";
 //const del = require("del");
 
 import { path } from "./gulp/config/path.js";
-import {plugins} from "./gulp/config/plugins.js";
+import { plugins } from "./gulp/config/plugins.js";
 import { res } from "./gulp/tasks/reset.js";
 import { copy } from "./gulp/tasks/copy.js";
 import { html } from "./gulp/tasks/html.js";
@@ -12,47 +12,35 @@ import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { server } from "./gulp/tasks/server.js";
 
-//import { otfToTtf, ttfToWoff, fontStyle } from "./gulp/tasks/fonts.js";
+import { ttfToWoff } from "./gulp/tasks/fonts.js";
 
 global.app = {
   path: path,
   gulp: gulp,
-  plugins: plugins,
+  plugins: plugins
 };
 
-//const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle);
+const fonts = gulp.series(ttfToWoff);
 
-const mainTasks = gulp.series(
-  //fonts,
-                              gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(gulp.parallel(copy, html, scss, js, images));
 
 const dev = gulp.series(res, mainTasks, gulp.parallel(server, watcher));
 
-function watcher(){
+function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.images, images);
 }
-//function clean() {
- // return del(['dist']);
- // console.log('--clean--');
-//}
 
-//function def() {
-//   console.log("default function is not ready yet");
-//  return null;
-//};
+gulp.task("default", dev);
 
-
-//exports.clean = clean;
-gulp.task('default', dev);
-
-gulp.task('del', res);
-gulp.task('copy', copy);
-gulp.task('watcher', watcher);
-gulp.task('dev', dev);
-gulp.task('html', html);
-gulp.task('css', scss);
-gulp.task('img', images);
+gulp.task("del", res);
+gulp.task("copy", copy);
+gulp.task("watcher", watcher);
+gulp.task("dev", dev);
+gulp.task("html", html);
+gulp.task("css", scss);
+gulp.task("img", images);
+gulp.task("fonts", fonts);
